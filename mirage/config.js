8f8f100ff0ca7b1189046a144560e8197c9666b3
Jure Cuhalev <gandalfar@gmail.com>
2016-06-16 13:19:52 +0200

    Correctly mock error with mirage

--- mirage/config.js
+++ mirage/config.js
@@ -1,3 +1,5 @@
+import Response from 'ember-cli-mirage/response';
+
 export default function() {
 
   // These comments are here to help you get started. Feel free to delete them.
@@ -9,18 +11,39 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
+  this.namespace = '/api';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
-  /*
-    Shorthand cheatsheet:
+ this.post('registrations', () => {
+  // return {
+  //   errors: {
+  //     'firstname': ['Must be present'],
+  //     'lastname': ['Numbers are not allowed']
+  //   }
+  // };
 
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
+  return new Response(422, {},
+    {
+      "errors": [
+        {
+          "source": { "pointer": "/data/attributes/firstname"},
+          "title": "Name is too short",
+          "detail": "We enforce long names"
+        }
+      ]
+    }
+  );
 
-    http://www.ember-cli-mirage.com/docs/v0.2.0-beta.7/shorthands/
-  */
+  // return new Response(200, {"header": "two"}, {
+  //  "data": {
+  //    "type": 'registration',
+  //    "id": 1,
+  //    "attributes": {
+  //      "firstname": 'Mark',
+  //      "lastname": 'Stone'
+  //    }
+  //  }
+  // });
+
+ });
 }
